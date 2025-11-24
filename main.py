c29e8d4d0f7b7b41e9bc388da53461de0fa5b529
Airamik <98848345+Airamik@users.noreply.github.com>
2025-11-24 19:51:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from moods import (
     day_mood, set_enabled, is_enabled
 )
 import threading
+import base64
 
 openlab = tuke_openlab.Controller(tuke_openlab.simulation_env("mg383jw"))
 
@@ -11,12 +12,28 @@ openlab = tuke_openlab.Controller(tuke_openlab.simulation_env("mg383jw"))
 current_thread = None
 
 
+# -------------------------------------------
+# LOAD IMAGE AS BASE64
+# -------------------------------------------
+def load_image_b64(filename):
+    with open(filename, "rb") as f:
+        return base64.b64encode(f.read()).decode("utf-8")
+
+
+# -------------------------------------------
+# SHOW IMAGE ON SCREEN
+# -------------------------------------------
+def show_image(filename):
+    img_b64 = load_image_b64(filename)
+    openlab.screens.show_image(img_b64)
+
+
 # ---------------------------------------------------
 # STOP FUNCTION – zastaví animáciu
 # ---------------------------------------------------
 def stop_effect():
     global current_thread
-    set_enabled(False)      # zastaviť slučky
+    set_enabled(False)
     openlab.lights.turn_off()
 
     if current_thread and current_thread.is_alive():
@@ -31,8 +48,8 @@ def stop_effect():
 def start_new_effect(target_fn):
     global current_thread
 
-    stop_effect()           # stop before starting new
-    set_enabled(True)       # povoliť beh efektu
+    stop_effect()
+    set_enabled(True)
 
     current_thread = threading.Thread(target=target_fn)
     current_thread.start()
@@ -46,22 +63,28 @@ def on_speech(text: str):
 
     if text in ["koniec", "stop"]:
         stop_effect()
+        openlab.screens.clear()
         return
 
     elif text == "jar":
+        show_image("jar.png")
         start_new_effect(run_spring_pulse)
 
     elif text == "leto":
+        show_image("leto.png")
         start_new_effect(run_summer_pulse)
 
     elif text == "jeseň":
+        show_image("jesen.png")
         start_new_effect(run_autumn_pulse)
 
     elif text == "zima":
+        show_image("zima.png")
         start_new_effect(run_winter_pulse)
 
     elif text == "deň" or text == "default":
         stop_effect()
+        openlab.screens.clear()
         day_mood()
 
 
